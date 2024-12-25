d2a883b3801a7292c39ca35fe5e01256f7a52878
tiwariparth <243mjparth@gmail.com>
2024-12-25 17:13:19 +0530

    remove .dockerignore, update logging in index.js to include app name, and add docker-compose.yml for multi-container setup

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const path = require("path");
 
 const app = express();
 
+const replicaApp = process.env.APP_NAME;
+
 app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
-  console.log("Request served");
+  console.log(`Request received from ${replicaApp}`);
 });
-
+ 
 app.listen(3000, () => {
-  console.log("Server started");
+  console.log(`Server started on${replicaApp}`);
 });
